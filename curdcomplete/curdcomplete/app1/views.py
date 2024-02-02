d27571d7cc7cc1260d740a4eb09b8f09ecb463e3
7460User <158508548+7460User@users.noreply.github.com>
2024-02-02 11:12:07 +0500

    CURD

---
+++ curdcomplete/curdcomplete/app1/views.py
@@ -0,0 +1,44 @@
+from multiprocessing import context
+from django.shortcuts import render,redirect
+from .forms import EmployeeForm
+from .models import Employee
+
+
+# Create your views here.
+def Home(request):
+    form=EmployeeForm()
+    if request.method=='POST':
+        form=EmployeeForm(request.POST)
+        form.save()
+        form=EmployeeForm()
+    data=Employee.objects.all()
+    context={
+        'form':form,
+        'data':data,
+    }
+   
+    return render(request,'app1/index.html',context)
+
+# Delete Views Records
+
+def Delete_record(request,id):
+    a=Employee.objects.get(pk=id)
+    a.delete()
+    return redirect('/')
+
+# update Record
+def Update_record(request,id):
+    if request.method=='POST':
+         data=Employee.objects.get(pk=id)
+         form=EmployeeForm(request.POST,instance=data)
+         if form.is_valid():
+             form.save()
+             return redirect('/')
+    else:  
+     data=Employee.objects.get(pk=id)
+     form=EmployeeForm(instance=data)
+    context={
+        'form':form,
+
+        }
+    return render(request,'app1/update.html',context)
